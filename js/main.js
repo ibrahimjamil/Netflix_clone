c03547f8b125844c12ddfaf32b55b090b93ec017
ibrahimjamil <ibrahimjamil147@ucp.edu.pk>
2020-11-01 03:21:29 +0500

    i add all netflix clone files here include html css and js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const tabitem=document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+function selectItem(){
+  removeBorder();
+  removeShow();
+  this.classList.add('tab-border');
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+	// Add show class
+	tabContentItem.classList.add('show');
+}
+function removeBorder(){
+  tabitem.forEach(item=>{
+    item.classList.remove('tab-border');
+  });
+}
+// Remove show class from all content items
+function removeShow() {
+	tabContentItems.forEach(item => {
+		item.classList.remove('show');
+	});
+}
+
+tabitem.forEach(item => {
+  item.addEventListener('click',selectItem);
+});
